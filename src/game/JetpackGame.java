a712f9f9fc5d456ba55ba348a5b7e1fdb548c593
ppedra <andrehagemann@hotmail.com>
2017-11-08 22:04:26 -0200

    gameloop agora diz oi4

--- src/game/JetpackGame.java
+++ src/game/JetpackGame.java
@@ -46,7 +46,7 @@ public class JetpackGame extends Game {
 	@Override
 	public void gameLoop() {
 
-		System.out.println("oi2");
+		System.out.println("oi4");
 		
 		switch (currentGameState) {
 		case MainMenu:
