353e54486fff2bdf812b048e719c2de2429e968f
Juliana Cristina Valenti <Juliana_valenti@estudante.sc.senai.br>
2017-12-06 20:43:23 -0200

    Missil colsiao corrigida

--- src/game/Missile.java
+++ src/game/Missile.java
@@ -39,14 +39,20 @@ public class Missile extends Enemy implements Interactable {
 			
 			switch (Utils.getInstance().collisionEnterIn(getRectangle(), objInCol.getRectangle())) {
 			case left_right:
+				super.setVelX(Math.abs(super.getVelX()));	
+				super.setTheta(super.getTheta() + (float) Math.PI);
+				break;
 			case right_left:
-				super.setVelX(super.getVelX() * (-1));	
-				super.setTheta(super.getTheta() + (float) -Math.PI);
+				super.setVelX(Math.abs(super.getVelX())*-1);	
+				super.setTheta(super.getTheta() - (float) Math.PI);
 				break;
 			case top_bot:
-			case bot_top:
-				super.setVelY(super.getVelY() * (-1));
+				super.setVelY(Math.abs(super.getVelY()));
 				super.setTheta(super.getTheta() + (float) Math.PI);
+				break;
+			case bot_top:
+				super.setVelY(Math.abs(super.getVelY())*-1);
+				super.setTheta(super.getTheta() - (float) Math.PI);
 				break;
 			default:
 				break;
